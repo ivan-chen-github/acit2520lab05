6a8b6ce01f76f476d47a78142cbef9a70d2757d4
Ivan <ichen48@my.bcit.ca>
2021-02-16 14:16:26 -0800

    Finished listquery

--- Movieflix_starter/app.js
+++ Movieflix_starter/app.js
@@ -11,21 +11,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+let movieList = ["test", "test2", "Gladiator"];
 
-app.get("/", (req, res) => res.render("pages/index"));
+app.get("/", (req, res) => res.render("pages/index", { movieList: movieList }));
 
 app.get("/myForm", (req, res) => res.render("pages/myForm"));
 
 app.post("/myForm", (req, res) => {
   let formData = req.body;
   console.log(formData);
-  let movieList = formData.movies.split(",")
-  console.log(movieList)
+  movieList = formData.movies.split(",");
+  console.log(movieList);
   res.render("pages/index", { movieList: movieList });
 });
 
 app.get("/myListQueryString", (req, res) => {
-  // Add your implementation here
+  let filteredList = [];
+  let { movie1, movie2 } = req.query;
+  
+  for (let i = 0; i < movieList.length; i++ ){
+    if ((movie1 == movieList[i].trim() || movie2 == movieList[i].trim() )) {
+      filteredList.push(movieList[i])
+    }
+  }
+  res.render("pages/index", { movieList: filteredList });
 });
 
 app.get("/search/:movieName", (req, res) => {
